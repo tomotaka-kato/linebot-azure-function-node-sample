5ca3985c38c1022af016652808f0e9c43caf4d4b
Tomotaka Kato <kato.tomotaka@smileforce.jp>
2019-08-06 12:03:33 +0900

    Azure Functions上で実行できるように変更

--- HttpTrigger/index.ts
+++ HttpTrigger/index.ts
@@ -1,21 +1,54 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import fetch = require( 'node-fetch');
+import line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
 
-    if (name) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello Hello!" + (req.query.name || req.body.name)
-        };
+
+    // パラメータ設定
+    const line_config = {
+        channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
+        channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
+    };
+
+    const qna_config = {
+        url: process.env.QnA_URL,
+        authorization: process.env.QnA_AUTHORIZATION
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+
+    const bot = new line.Client(line_config);
+
+     // 先行してLINE側にステータスコード200でレスポンスする。
+    context.res = { body: "" };
+
+    // イベントオブジェクトを順次処理。
+    req.body.events.forEach((event: any) => {
+        // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+        if (event.type == "message" && event.message.type == "text"){
+
+
+            const data = JSON.stringify({ question: event.message.text })
+
+            fetch(qna_config.url, {
+                method: "POST",
+                body: data,
+                headers: {
+                    "Authorization": qna_config.authorization,
+                    "Content-type": "application/json"
+                }
+            }).then((response: any) => response.json())
+            .then((json: any) => json.answers[0].answer)
+                .then((answer: any) => {
+                    // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+                    bot.replyMessage(event.replyToken, {
+                        type: "text",
+                        text: answer
+                    });
+                })
+
+        }
+    });
 };
 
 export default httpTrigger;
